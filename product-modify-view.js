3ee634536b7cb22a7b89385208b2b27f085920e1
Edward Seufert <edwardseufert@Edwards-MacBook-Pro.local>
2021-02-12 06:21:18 -0500

    ec modules

---
+++ product-modify-view.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import FormBuilder from '../../coreView/common/form-builder';
+
+export default function ECProductModifyView({itemState, appPrefs, 
+	onSave, onCancel, inputChange, onBlur}) {
+
+	let formName = "EC_PRODUCT_FORM";
+	let formTitle = "Products";
+	let formGroup = "FORM1";
+    
+    return (
+    	<FormBuilder itemState={itemState} formName={formName} formTitle={formTitle} formGroup={formGroup}
+    	appPrefs={appPrefs} onSave={onSave} onCancel={onCancel} inputChange={inputChange}/>
+    );
+}
+
+
+ECProductModifyView.propTypes = {
+  itemState: PropTypes.object.isRequired,
+  appPrefs: PropTypes.object.isRequired,
+  onSave: PropTypes.func,
+  onCancel: PropTypes.func,
+  inputChange: PropTypes.func,
+  onBlur: PropTypes.func
+};
